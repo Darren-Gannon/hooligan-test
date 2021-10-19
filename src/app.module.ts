f0d1607d69d8d83e157f031fc2ef4ea440d37708
Darren Gannon <darrengannon93@gmail.com>
2021-10-19 11:55:32 +0200

    feat: add configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppResolver } from './app.resolver';
 import { GQLDate } from './graphql/scaler/GQLDate';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { CurrentDatePubSub } from './current-date-pub-sub/current-date-pub-sub';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        VIDEO_LIMIT: Joi.number().default(3)
+      }),
+    }),
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
       playground: true,
