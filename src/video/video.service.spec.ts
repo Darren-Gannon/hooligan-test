484a1b1b30024ef02a21d3e5833fd6e30152a709
Darren Gannon <darrengannon93@gmail.com>
2021-10-19 14:51:30 +0200

    feat: add test for add video - service

--- src/video/video.service.spec.ts
+++ src/video/video.service.spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'express';
 import { Video } from './entities/video.entity';
 import { VideoService } from './video.service';
 
 describe('VideoService', () => {
   let service: VideoService;
+  const defaultUser: User = {
+    id: 'default'
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,7 +16,9 @@ describe('VideoService', () => {
         VideoService,
         {
           provide: getRepositoryToken(Video),
-          useValue: {}
+          useValue: {
+            save: async (video) => video
+          }
         }
       ],
     }).compile();
@@ -23,4 +29,15 @@ describe('VideoService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should add a video', () => {
+    service.create({
+      duration: 1,
+      name: ''
+    }, defaultUser).then(video => {
+      expect(video).toBeDefined()
+      expect(video.name).toBe('')
+      expect(video.duration).toBe(1)
+    })
+  })
 });
