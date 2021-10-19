7afc747a29e3ea0f8720b8e6c4b83c37706685b1
Darren Gannon <darrengannon93@gmail.com>
2021-10-19 14:17:03 +0200

    fix: swagger decorator for video id on create watch dto

--- src/watch/dto/create-watch.input.ts
+++ src/watch/dto/create-watch.input.ts
@@ -1,7 +1,9 @@
 import { InputType, Field, ID } from '@nestjs/graphql';
+import { ApiProperty } from '@nestjs/swagger';
 
 @InputType()
 export class CreateWatchInput {
+  @ApiProperty()
   @Field(() => ID)
   videoId: string;
 }
