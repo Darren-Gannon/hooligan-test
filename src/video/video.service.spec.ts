e9374c7678cd61ab80e57457b5e24d305d8ca1d9
Darren Gannon <darrengannon93@gmail.com>
2021-10-19 15:01:28 +0200

    feat: add create and update videos - service

--- src/video/video.service.spec.ts
+++ src/video/video.service.spec.ts
@@ -5,39 +5,94 @@ import { Video } from './entities/video.entity';
 import { VideoService } from './video.service';
 
 describe('VideoService', () => {
-  let service: VideoService;
   const defaultUser: User = {
     id: 'default'
   };
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        VideoService,
-        {
-          provide: getRepositoryToken(Video),
-          useValue: {
-            save: async (video) => video
+  describe('basic test', () => {
+    let service: VideoService;
+
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          VideoService,
+          {
+            provide: getRepositoryToken(Video),
+            useValue: {
+              save: async (video) => video
+            }
           }
-        }
-      ],
-    }).compile();
+        ],
+      }).compile();
 
-    service = module.get<VideoService>(VideoService);
+      service = module.get<VideoService>(VideoService);
+    });
+
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe('create video', () => {
+    let service: VideoService;
 
-  it('should add a video', () => {
-    service.create({
-      duration: 1,
-      name: ''
-    }, defaultUser).then(video => {
-      expect(video).toBeDefined()
-      expect(video.name).toBe('')
-      expect(video.duration).toBe(1)
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          VideoService,
+          {
+            provide: getRepositoryToken(Video),
+            useValue: {
+              save: async (video) => video
+            }
+          }
+        ],
+      }).compile();
+
+      service = module.get<VideoService>(VideoService);
+    });
+
+    it('should add a video', () => {
+      service.create({
+        duration: 1,
+        name: ''
+      }, defaultUser).then(video => {
+        expect(video).toBeDefined()
+        expect(video.name).toBe('')
+        expect(video.duration).toBe(1)
+      })
     })
-  })
+  });
+
+  describe('update video', () => {
+    let service: VideoService;
+
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          VideoService,
+          {
+            provide: getRepositoryToken(Video),
+            useValue: {
+              save: async (video) => video
+            }
+          }
+        ],
+      }).compile();
+
+      service = module.get<VideoService>(VideoService);
+    });
+
+    it('should update a video', () => {
+      service.update('1', {
+        duration: 1,
+        name: ''
+      }, defaultUser).then(video => {
+        expect(video).toBeDefined()
+        expect(video.name).toBe('')
+        expect(video.duration).toBe(1)
+      })
+    })
+  });
 });
